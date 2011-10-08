30eb8d3f23318eb644660388a8e70f84e5d51a3d
Joost Schuur <jschuur@jschuur.com>
2011-10-08 16:36:44 -0700

    Default RESTful routes for tweets; Gmail style button gradients.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Tweetlater::Application.routes.draw do
   root :to => 'tweets#index', :as => :root
 
-  resources :users
-
   match "auth/twitter/callback" => "sessions#create"
   match "auth/failure" => "sessions#error"
   match "signout" => "sessions#destroy", :as => :signout
   match "settings" => "users#edit", :as => :settings
-
-  match "tweets/edit/:id" => "tweets#edit", :as => :edit_tweet
-  match "tweets/create" => "tweets#create", :as => :tweets
-  match "tweets/destroy/:id" => "tweets#destroy", :as => :destroy_tweet
   match "tweets/shorten" => "tweets#shorten", :as => :shorten_link
+
+  resources :tweets, :users
 end
