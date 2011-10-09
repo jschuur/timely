dbebdbf6bf6e2b42971aa3a45c06a142ffa7d71e
Joost Schuur <jschuur@jschuur.com>
2011-10-08 17:08:14 -0700

    Production Twitter credentials.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development?
     provider :twitter, 'd2p0K6e4xNUZituIpeNtYw', 'mUUPI64jirJoOln5G0jTsyudT0Gs8Z5gcDB16DZEg'
   else
-    provider :twitter, '', ''
+    provider :twitter, 'nS2HuAVtFGqrHOrtok6KA', '4WQpXJjT45ov0NrEAprgSb762nFkhQvbflfDBcbhpk'
   end
 end
